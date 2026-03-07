ede8c52c5a4aacc8945ccfa25d3bd88cab073c0c
NAIMUR RAHMAN <rnaimur3386@gmail.com>
2026-03-07 13:50:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
+const typingAnimationElement = document.getElementById('typing-animation');
+
 const words = ["Power BI Developer", "SQL Analyst", "Python Programmer", "Data Enthusiast"];
-let i = 0;
-let timer;
+let wordIndex = 0;
+let charIndex = 0;
 
-function typingEffect() {
-    let word = words[i].split("");
-    let loopTyping = function() {
-        if(word.length > 0) {
-            document.getElementById('typing-container').innerHTML += word.shift();
-        } else {
-            setTimeout(deletingEffect, 1500);
-            return false;
-        }
-        timer = setTimeout(loopTyping, 150);
-    };
-    loopTyping();
+function type() {
+  if (charIndex < words[wordIndex].length) {
+    typingAnimationElement.textContent += words[wordIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 150);
+  } else {
+    setTimeout(erase, 1000);
+  }
 }
 
-function deletingEffect() {
-    let word = document.getElementById('typing-container').innerHTML.split("");
-    let loopDeleting = function() {
-        if(word.length > 0) {
-            word.pop();
-            document.getElementById('typing-container').innerHTML = word.join("");
-        } else {
-            i = (i + 1) % words.length;
-            typingEffect();
-            return false;
-        }
-        timer = setTimeout(loopDeleting, 100);
-    };
-    loopDeleting();
+function erase() {
+  if (charIndex > 0) {
+    typingAnimationElement.textContent = words[wordIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 70);
+  } else {
+    wordIndex = (wordIndex + 1) % words.length;
+    setTimeout(type, 500);
+  }
 }
 
-typingEffect();
+type();
+
+// Navbar active link color update
+const navLinks = document.querySelectorAll('.navbar ul li a');
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.forEach(l => l.classList.remove('active'));
+    link.classList.add('active');
+  });
+});
